670f4f8fd5ca2a8d5dc976c3c2133740cadd8523
Sipeng He <hsp8412@hotmail.com>
2022-01-25 02:12:15 -0700

    Create the controller and dishService

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -4,15 +4,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Restaurant.Models
 {
-	public class Dish
-	{
-		public int Id { get; set; }
+    public class Dish
+    {
+        public int Id { get; set; }
 
-		[Required]
-		public string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
 
-		[Column(TypeName = "decimal(18,2)")]
-		public decimal Price { get; set; }
-	}
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public Dish(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
 }
 
